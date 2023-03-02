32569b54f1337d3d7144d8882b8e63ad32f2f0ab
tomyferra <ferra.tomy@gmail.com>
2023-03-02 14:00:06 -0300

    removed project

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -2,13 +2,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const ProjectsData = [
 {
-  name: 'Sticky Notes App',
-  summary: 'Write down notes, complete them and clear them',
-  url: 'https://tomyferra.github.io/stickyNotesApp/',
-  urlgithub: 'https://github.com/tomyferra/stickyNotesApp',
-  id: uuidv4()
-},
-{
   name: 'Crypto Converter',
   summary: 'This is a project where you input an amount of BTC and ig tives you the equivalence in other coins as well as the total price',
   url: 'https://tomyferra.github.io/cryptoconverter/',
