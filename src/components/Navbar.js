313fad3d4cda2d447bbf32d78f0472e9c24253f6
tomyferra <ferra.tomy@gmail.com>
2023-03-02 13:39:19 -0300

    links updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ function NavigationBar () {
         <div className="collapse navbar-collapse" id="navbar-toggler">
         <a className="navbar-brand" href="#home"><img className="navbar-brand" src={require('../images/TomasFerrari.gif')} alt="tomas Ferrari"></img></a>
           <ul className="navbar-nav d-flex justify-content-center align-items-center">
-          <li class="nav-item">
+            <li class="nav-item">
               <a class="nav-link" aria-current="page" href="#About">About Me</a>
             </li>
             <li class="nav-item">
