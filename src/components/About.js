f4c44389e3eabe6d5c94d55ed81fb0fc8bbb3eeb
tomyferra <ferra.tomy@gmail.com>
2024-01-24 11:08:19 -0300

    fixing issues

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About () {
       <div className="container col col-md-7 about-introduction align-items-center justify-content-center">
         <div className="row py-10">
           <h1 className="text-center" >Hi, I'm Tomas Ferrari.</h1>
-          <h2 className="text-center">Software Developer</h2>
+          <h2 className="text-center">Software Developer.</h2>
         </div>
         <div className="row">
           <h4 className="text-secondary text-center">I am an experienced backend developer with 5 years doing Python and SQL, complemented by 3 years of front-end work in React, Bootstrap, Tailwind and JavaScript. Aspiring to become a full-stack developer driven to fuse technical depth with creative frontend solutions. Open to relocation for the right opportunity. Eager to contrinute my expertise to innovative projects and expand my skill set.</h4>
